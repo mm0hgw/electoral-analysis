80bfa3c7c38b7de65d681dcd2f1b17fd9636f124
Dale <dale@piratepress.org>
2015-06-05 16:08:07 +0100

    fixed display

--- mod/display.r
+++ mod/display.r
@@ -22,9 +22,15 @@ mean_sd_lines <- function(sample,p_mean=mean(sample),p_sd=sd(sample)){
 }
  
 #calculate separate sds for lhs / rhs of peak density and display
-peak_separate_sd_lines<-function(i,i_peak){
-        i_l_sd <- custom_sd(i[i<i_peak],i_peak)
-        i_r_sd <- custom_sd(i[i>i_peak],i_peak)
+peak_separate_sd_lines<-function(i,i_peak,separate_sd=F){
+	if(separate_sd==T){
+        	i_l_sd <- custom_sd(i[i<i_peak],i_peak)
+        	i_r_sd <- custom_sd(i[i>i_peak],i_peak)
+	}else{
+		i_sd <- custom_sd(i,i_peak)
+		i_l_sd <- i_sd
+		i_r_sd <- i_sd
+	}
         abline(v=i_peak,col="green")
         abline(v=i_peak+i_r_sd,col="blue")
         abline(v=i_peak-i_l_sd,col="blue")
@@ -48,6 +54,7 @@ custom_plot_ecdf<-function(bucket,match_pattern,do.density=T,do.cdf=T){
         i_mean <- mean(intercept)
         i_sd <- custom_sd(intercept,i_mean)
         i_dev <- deviation_in_SDs(i_match,i_mean,i_sd)
+	print(i_dev)
         main <- paste("Matching \"",match_pattern,"\"",sep="")
         sub <- paste("Matched sample ranges from ",sprintf("%.2f",min(i_dev)),
                 " to ",sprintf("%.2f",max(i_dev)),
@@ -55,27 +62,19 @@ custom_plot_ecdf<-function(bucket,match_pattern,do.density=T,do.cdf=T){
                 # do density plot
         if(do.density==T){
                 plot(density_obj,main=paste(main,"population mean / cdf intercept error density"),sub=sub)
-                lines(x=i_x,dnorm(x=i_x,mean=i_peak,sd=custom_sd(intercept,i_peak)),col="magenta") 
-                peak_separate_sd_lines(intercept,i_peak)
-                points(x=i_match,y=dnorm(x=i_match,mean=i_peak,sd=i_sd),pch=1,col="red")
+                lines(x=i_x,dnorm(x=i_x,mean=i_mean,sd=i_sd),col="magenta") 
+                peak_separate_sd_lines(intercept,i_mean)
+                points(x=i_match,y=dnorm(x=i_match,mean=i_mean,sd=i_sd),pch=1,col="red")
         }
                 # do c.d.f. plot
         if(do.cdf==T){
                 plot(ecdf_obj,verticals=T,do.points=F,main=paste(main,"population mean / cdf intercept error c.d.f."),sub=sub)
-                lines(x=i_x,pnorm(q=i_x,mean=i_mean,sd=sd(intercept)),col="magenta")
-                points(x=i_match,y=pnorm(q=i_match,mean=i_mean,sd=custom_sd(intercept,i_mean)),pch=1,col="red")
+                lines(x=i_x,pnorm(q=i_x,mean=i_mean,sd=i_sd),col="magenta")
+                points(x=i_match,y=pnorm(q=i_match,mean=i_mean,sd=i_sd),pch=1,col="red")
                 peak_separate_sd_lines(intercept,i_mean)
                 abline(h=0.5)
         }
         return(i_dev)
-}
-
-
-do_display<-function(match_pattern="SIR2014",sample=total_sample()){
-        pdf_target(paste("turnout_analysis_",match_pattern,sep=""))
-        out<-custom_plot_ecdf(sample,match_pattern)
-        dev.off()
-        return(out)
 }
 
 single_display<-function(sample){
--- mod/import.r
+++ mod/import.r
@@ -3,6 +3,98 @@
 #
 require(foreach)
 
+# key constant for grepping files
+LE2014_key <- c("Ward",
+	"Local Authority",
+	"Electorate",
+	"Total votes cast",
+	"Number of postal ballot papers issued",
+	"Number of postal votes included in the count")
+
+custom_key <- c("^name$",
+	"^Region$",
+	"^N$",
+	"^V$",
+	"^NP$",
+	"^VP$")
+
+keys <- cbind(LE2014_key,
+	custom_key)
+
+# read headers
+read_headers <- function(filename){
+	p<-read.csv(filename,header=F,stringsAsFactors=F)[c(1,2),]
+	return(p)
+}
+
+# rbind into table and return maximums from columns
+key_build <- function(...){
+	p<-rbind(...)
+	l_len<-length(p[1,])
+	if(l_len==0){
+		return()
+	}
+	out<-foreach(l=seq(1,l_len),.combine=c,.multicombine=T,.inorder=T)%do%{
+		max(p[,l])
+	}
+	out
+}
+
+# grep headers, find key
+try_key <- function(filename,key){
+	p<-read_headers(filename)
+	l_len<-seq(1,length(p[,1]))
+	foreach(l=l_len,.combine=key_build,.multicombine=T,.inorder=F)%:%
+	foreach(k=key,.combine=c,.multicombine=T,.inorder=T)%do%{
+		match<-(grep(pattern=k,x=p[l,]))
+		if(any(match)){
+			min(match)
+		}else{
+			0
+		}
+	}
+}
+
+# identify a valid key. Keys are valid without a Region field
+valid_key <- function(key){
+	if(key[1]!=0&key[2]!=0&key[3]!=0&key[4]!=0&key[5]!=0&key[6]!=0){
+		return(TRUE)
+	}
+	return(FALSE)
+}
+
+# collect valid keys
+key_collect<-function(...){
+	p<-rbind(...)
+	p_len<-length(p[,1])
+	if(p_len>0){
+		mask<-foreach(p_id=seq(1,p_len),.combine=c,.multicombine=T,.inorder=T)%do%{
+			valid_key(p[p_id,])
+		}
+		return(p[mask,])
+	}
+	return(vector())
+}
+
+# find valid keys for provided files
+find_key <- function(filenames){
+	k_len<-length(keys[1,])
+	foreach(file=filenames,.combine=list,.multicombine=T,.inorder=T)%:%
+	foreach(key_id=seq(1,k_len),.inorder=F,.combine=key_collect,.multicombine=T)%do%{
+		k<-try_key(file,keys[,key_id])
+		if(valid_key(k)==T){
+			k
+		}else{
+			0
+		}
+	}
+}
+
+# list all csv files
+list_csv_files <- function(){
+	paste(sep="","csv/",list.files(path="csv/",pattern=".csv$"))
+}
+
 # read custom scrubbed format
 read_custom_csv <- function(file){
         bal<-read.csv(file)
@@ -30,6 +122,23 @@ read_custom_csv <- function(file){
 find_LE2014_files<-function(){
 	p<-sub(list.files(path="csv/",pattern="Local elections 2014"),pattern="^",replacement="csv/")
 	return(p)
+}
+
+assemble_sample <- function(){
+	files<-list_csv_files()
+	file_keys<-find_key(files)
+	l_files<-length(files)
+	if(l_files>0){
+		foreach(id=seq(1,l_files),.combine=rbind,.multicombine=T)%do%{
+			key<-file_keys[[id]]
+			out<-vector()
+			if(valid_key(key)==T){
+				file<-files[[id]]
+				out<-read_LE2014_ballot(file,key)
+			}
+			out
+		}
+	}
 }
 
 
@@ -64,21 +173,19 @@ read_all_LE2014_files<-function(){
 	foreach(n=p,
 		.combine=rbind,
 		.inorder=F,
-		.multicombine=T)%dopar%{
+		.multicombine=T)%do%{
 		read_LE2014_ballot(n)
 	}
 }
 
 # import LE2014 ballot file
-read_LE2014_ballot <- function(file){
-	key <- find_LE2014_key(file)
-	if(sum(key==0)>0){
+read_LE2014_ballot <- function(file,key=find_LE2014_key(file)){
+	if(!valid_key(key)){
 		return()
 	}
         p<-read.csv(file,strip.white=T,sep=",",stringsAsFactors=F,header=T)[,key]
-	i<-p[,1]!=""
         p2<-p[p[,1]!=""&p[,3]!=""&p[,3]!="0",]
-	
+
         ballot<-list(
                 name=p2[,1],
                 Region=as.factor(p2[,2]),
@@ -86,6 +193,7 @@ read_LE2014_ballot <- function(file){
                 V=really_strip_whitespace(p2[,4]),
                 NP=really_strip_whitespace(p2[,5]),
                 VP=really_strip_whitespace(p2[,6]))
+	print(ballot)
         return(cook_ballot(ballot,file))
 }
 
@@ -95,7 +203,7 @@ read_all_custom_csv<-function(){
 	q<-foreach(n=grep(p,pattern=".csv$",value=T),
 		.combine=rbind,
 		.inorder=F,
-		.multicombine=T)%dopar%{
+		.multicombine=T)%do%{
 		read_custom_csv(n)
 	}
 	return(q)
