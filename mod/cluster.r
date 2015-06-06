ddc4ea31b7a638e098f4e224b7abc04c75aa73de
Dale <dale@piratepress.org>
2015-06-06 16:20:44 +0100

    more cluster tweaks

---
+++ mod/cluster.r
@@ -0,0 +1,34 @@
+#
+#	Cluster setup
+#
+
+require(parallel)
+require(doParallel)
+
+# setup parallelisation parameters
+no_cores <- detectCores()
+mcoptions <- list(preschedule=F,
+	set.seed=F,
+	silent=T,
+	cores=no_cores)
+
+# make custom fork cluster
+makeCustomCluster <- function(){
+	cl<-makeForkCluster(nnodes=no_cores,nocompile=T)
+	registerDoParallel(cl=cl,cores=no_cores)
+	cl
+}
+
+# switch to sequential processing
+set_sequential <- function(){
+	registerDoSEQ()
+}
+
+# switch to parallel processing
+set_parallel <- function(){
+	if(exists("cl")){
+		registerDoParallel(cl=cl,cores=no_cores)
+	}else{
+		print("no cluster detected")
+	}
+}
