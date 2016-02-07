b006d4bc19368128f612b4cbf58e12d967401090
Dale <dale@piratepress.org>
2016-02-07 05:13:05 +0000

    contiguity

--- old/density_scatter.r
+++ old/density_scatter.r
@@ -56,7 +56,8 @@ contiguous_check  <- function(
 	target <- rep(FALSE,ncol(t))
 	target[x] <- TRUE
 	g <- t[x[1],]&target
-	print(sum(xor(g,target))>0){
+	print(sum(xor(g,target))>0)
+if(TRUE){
 		h<-target&col_or(t[g,])
    	print(h)
 		if(sum(xor (g ,h))== 0){
