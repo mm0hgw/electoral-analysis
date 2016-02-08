153b21fc22b4f84e95d29a63f9494a2c9c3966a3
Dale <dale@piratepress.org>
2016-02-08 00:37:40 +0000

    reorganize

--- old/mod/calculating.r
+++ old/mod/calculating.r
@@ -9,6 +9,11 @@ sigma_to_probability <- function(x){
         Re(erfc(x/sqrt(2)))
 }
 
+# cdf / mean intercept
+cdf_mean_intercept<-function(V,N){
+	sum((V/N)<(sum(V)/sum(N)))/length(N)
+}
+
 #sort sample by decreasing deviation
 sort_sample <- function(sample){
 	o<-as.numeric(sample)
@@ -40,6 +45,7 @@ calculate_np <- function(total,postal){
 	out
 }
 
+# calculate normalised turnout
 calculate_normalised_a <- function(V,N){
 	a<-calculate_a(V,N)
 	pop_mean <- calculate_a(sum(V),sum(N))
@@ -47,15 +53,6 @@ calculate_normalised_a <- function(V,N){
 	(a-pop_mean)/pop_sd
 }
 
-normalised_a <- function(b){
-	total<- calculate_normalised_a(b$V,b$N)
-	mask <- !is.na(b$NP)
-	postal<- calculate_normalised_a(b$VP[mask],b$NP[mask])
-	nonp <- calculate_normalised_a(
-		calculate_np(b$V[mask],b$VP[mask]),
-		calculate_np(b$N[mask],b$NP[mask]))
-	list(total,postal,nonp)
-}
 
 # cook a list of files using the normalised turnout technique
 cook_files <- function(files){
@@ -72,37 +69,6 @@ cook_files <- function(files){
 	}
 }
 
-#remove single ballot
-remove_ballot <- function(x,n=x$N==min(x$N[calculate_a(x$V,x$N)>(calculate_a(sum(x$V),sum(x$N)))])){
-	x$V[n] <- x$V[n]-1
-	x
-}
-
-#remove multiple ballots
-remove_ballots <- function(x){
-	while(mean(calculate_a(x$V,x$N))>calculate_a(sum(x$V),sum(x$N))){
-		x<-remove_ballot(x)
-	}
-	x
-}
-
-# calculate cdf intercept  
-cdf_intercept <- function(sample,mean){
-        if(length(sample)>0){
-                return(sum(sample<mean)/length(sample))
-        }
-        return()
-}
-
-#given a matrix with N,V,a rows, compute population mean, compare turnout vector to mean
-#count occurances and divide by vector length
-cdf_mean_intercept<-function(bal){
-	if(length(bal["N",])==0){ # trap zero division
-		return()
-	}
-        out<-sum(bal["a",]< calculate_a(sum(bal["V",]),sum(bal["N",]))) / length(bal["N",])
-	return(out)
-}
 
 # find a local maximum by iteration.
 first_local_maximum <- function(density_obj,from=min(density_obj$x)){
@@ -137,80 +103,3 @@ first_local_maximum <- function(density_obj,from=min(density_obj$x)){
 deviation_in_SDs <- function(x,x_mean=mean(x),x_sd=sd(x)){
         abs((x-x_mean)/x_sd)
 }
-
-# calculate cdf/mean intercepts for ballot and subregions
-results_by_region <- function(bal,tag,title){
-		# identify any subregions
-        regions<-levels(as.factor(bal$Region))
-	if(sum(regions=="")==0){
-		regions<-c("",regions)
-	}
-		# for each sub region, run cdf_mean_intercept, seed results with whole set
-	FUN<-function(r){
-		if(r==""){
-			a<-bal$ballots
-		}else{
-			a<-bal$ballots[,bal$Region==r]
-		}
-		mask<-is.na(a["N",])
-		b<-a[,!mask]
-		if(length(b["N",b["N",]!=0])<=2|sum(b["N",])==0){
-			return(-1)
-		}
-		out<-cdf_mean_intercept(b)
-		if(is.na(out)){
-			return(-1)
-		}
-                out
-	}
-	out_names<-paste(title,regions,tag)
-	out<-foreach(r=regions,
-		.inorder=FALSE,
-		.combine=c,
-		.multicombine=TRUE,
-		.options.multicore=mcoptions
-	)%dopar%{
-		FUN(r)
-	}
-	names(out)=out_names
-	out<-out[out!=-1]
-        return(out)
-}
-
-# how we like our ballots cooked for easy grepping
-cook_ballot <- function(ballot,title){
-        ballot$NNP<-calculate_np(ballot$N,ballot$NP)
-        ballot$VNP<-calculate_np(ballot$V,ballot$VP)
-        ballot$a<-calculate_a(ballot$V,ballot$N)
-        ballot$ap<-calculate_a(ballot$VP,ballot$NP)
-        ballot$anp<-calculate_a(ballot$VNP,ballot$NNP)
-
-        bal=rbind(N=ballot$N,
-                        V=ballot$V,
-                        a=ballot$a)
-        balp=rbind(N=ballot$NP,
-                        V=ballot$VP,
-                        a=ballot$ap)
-        balnp=rbind(N=ballot$NNP,   
-                        V=ballot$VNP,
-                        a=ballot$anp)
-
-        objt<-list(
-                name=ballot$name,
-                Region=ballot$Region,
-                ballots=bal)
-        objp<-list(
-                name=ballot$name,
-                Region=ballot$Region,
-                ballots=balp)
-        objnp<-list(
-                name=ballot$name,
-                Region=ballot$Region,
-                ballots=balnp)
-	out<-results_by_region(objt,"total",title)
-	outp<-results_by_region(objp,"postal",title)
-	outnp<-results_by_region(objnp,"non-p",title)
-
-        return(c(out,outp,outnp))
-}
-
