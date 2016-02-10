63b445145abda7f3ae571f3cc5d9cf37e91c0f04
Dale <dale@piratepress.org>
2016-02-10 21:54:09 +0000

    b

---
+++ old/mod/combnParallel.r
@@ -0,0 +1,40 @@
+
+# choose look up table gen
+chooseLutGen <- function(n,k){
+	out<-matrix(1,nrow=n,ncol=k)
+	foreach(i=icount(n))%:%
+	foreach(j=icount(k))%do%{
+		if(i>j){
+			out[i,j]<-choose(i,j)
+		}
+	}
+}
+
+#combination generator generator
+combnGenGen <- function(n,k){
+	chooseLut <- chooseLutGen(n-1,k-1)
+	combnGen <- function(x,n=n,k=k){
+		if(k==1){return(x)}
+		cnk<-chooseLut[n-1,k-1]
+		if(x<=cnk){
+			out<-c(1,combnGen(x,n-1,k-1)+1)
+		}else{
+			out<-combn(x-cnk,n-1,k)+1
+		}
+		out
+	}
+}
+
+# unit test for combination generator
+testCombnGen<-function(n,k){
+	tgt<-combn(n,k)
+	combnGen<-combnGenGen(n,k)
+	foreach(
+		i=icount(choose(n,k)),
+		.combine=sum
+	)%dopar%{
+		tgt[,i]!=combnGen(i)
+	}
+}
+
+
