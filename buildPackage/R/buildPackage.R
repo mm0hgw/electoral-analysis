41f127a1118c0f300116fb013437e8dcdc14c8f4
Dale <dale@piratepress.org>
2016-02-14 14:43:52 +0000

    h h

---
+++ buildPackage/R/buildPackage.R
@@ -0,0 +1,23 @@
+require(devtools)
+
+gitPull<- function(){
+	system2("git",c("pull"))
+}
+
+findPackages <- function(path="."){
+	l<-gsub("./","",list.dirs(path=path))
+	print(l[unlist(lapply(paste(l,"/NAMESPACE",sep=""),file.exists))])
+	
+}
+
+buildPackage <- function(package){
+	document(package)
+	system2("git",c("add", paste(package,"/man/*",sep="")))
+	system2("R",c("CMD","check",package))
+	system2("R",c("CMD","build",package))
+	install.packages(paste(package,"_1.0.tar.gz",sep=""))
+}
+
+buildPackages<-function(packages=findPackages()){
+	lapply(packages,buildPackage)
+}
