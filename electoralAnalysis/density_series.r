96b42adbe32690ee85fbf4a3f91a34e9d10a9756
Dale <dale@piratepress.org>
2016-09-14 11:55:13 +0100

    still sprinting

--- electoralAnalysis/density_series.r
+++ electoralAnalysis/density_series.r
@@ -1,104 +1,150 @@
 # setup data read options
 require(moments)
+require(foreach)
 
-SC_GE_fn <- function(b){
-	list(overall.turnout=b$V,
-		SNP.turnout=b$SNP,
-		nonSNP.turnout=b$V-b$SNP
-	)
-}
+	# takes a single ballot
+	# returns a list of FUN(SAMPLE_FUN()) results applied to the ballot
 
-SC_GE_tag_fn <- function(GE,name,FUN){
-	f<-a_fn_fn(GE$N)
-	out<-lapply(
-		SC_GE_fn(GE),
-		function(b){FUN(f(b))}
+ballot_funk<-function(b,
+	SAMPLE_FUN=calculate_normalised_a,
+	FUN=density,
+		# pull names from ballot
+	i_names=colnames(b)[(colnames(b)!="N")&(sapply(b[1,],is.numeric))],
+		# shift names to human readable
+	o_names=gsub("^V$","overall",i_names)	
+){
+		# apply FUN to SAMPLE_FUN over named elements
+	out<-lapply(i_names,
+		function(n){
+			FUN(SAMPLE_FUN(b[,n],b$N))
+		}
 	)
-	names(out) <- gsub("^",paste(name,".",sep=""),names(out))
+		# tag results
+	names(out)<-paste(o_names,".turnout",sep="")
+		# return
 	out
 }
 
-SC_GE_list_fn <- function(SC_GE_list,FUN=density){
-	foreach(GE=SC_GE_list,n=names(SC_GE_list),.combine=c)%do%{
-		out<-SC_GE_tag_fn(GE,n,FUN)
+ballot_list_funk<-function(b_list,
+	SAMPLE_FUN=calculate_normalised_a,
+	FUN=density
+){
+	foreach(b=b_list,
+		n=names(b_list),
+		.combine=c
+	)%do%{
+		out<-ballot_funk(b,
+			SAMPLE_FUN=SAMPLE_FUN,
+			FUN=FUN
+		)
+		names(out)<-paste(n,".",names(out),sep="")
+		out
 	}
 }
 
-SC_GE_list <- list(GE2005S=GE2005S,
+SC_list <- list(GE2005S=GE2005S,
 	GE2010S=GE2010S,
 	GE2015S=GE2015S,
 	SP2007=SP2007,
 	SP2011=SP2011,
-	SP2016=SP2016
+	SP2016=SP2016,
+	SIR2014=SIR2014,
+	VSR2011S=VSR2011S,
+	EUR2016S=EUR2016S
 )
 
-SC_GE_skewness <- unlist(SC_GE_list_fn(SC_GE_list,FUN=skewness))
-
-S_fn <- a_fn_fn(SIR2014$N)
-
-SIR_fn <-function(FUN){ list(SIR2014.overall.turnout=FUN(S_fn(SIR2014$V)),
-	SIR2014.Yes.turnout=FUN(S_fn(SIR2014$Yes)),
-	SIR2014.No.turnout=FUN(S_fn(SIR2014$No))
-)}
-
-SIR_sum<-c(SIR2014.overall.turnout=sum(SIR2014$V),
-	SIR2014.Yes.turnout=sum(SIR2014$Yes),
-	SIR2014.No.turnout=sum(SIR2014$No)
-)
-
-SIR_a<-c(SIR2014.overall.turnout=sum(SIR2014$V)/sum(SIR2014$N),
-	SIR2014.Yes.turnout=sum(SIR2014$Yes)/sum(SIR2014$N),
-	SIR2014.No.turnout=sum(SIR2014$No)/sum(SIR2014$N)
-)
-
-
-SIR<-SIR_fn(density)
-SIR_skewness<-SIR_fn(skewness)
-
-SC_GE<-SC_GE_list_fn(SC_GE_list)
-
-SC_d_list<-c(SIR,SC_GE)
-SC_d_skewness<-unlist(c(SIR_skewness,SC_GE_skewness))
-
-chisq_fn <- function(d){
-	sum((d$y-dnorm(d$x))^2)
-}
-
-SC_d_chisq<-unlist(lapply(SC_d_list,chisq_fn))
-
-SC_sum_fn<-function(GE){
-	out<-colSums(GE[,c("V","SNP")])
-	out<-c(out,out["V"]-out["SNP"])
-	names(out)[3]<-"nonSNP"
-	out
-}
-
-SC_GE_sum<-foreach(d=lapply(SC_GE_list,SC_sum_fn),n=names(SC_GE_list),.combine=c)%do%{ 
-	out<-d
-	names(out)<-gsub("^",paste(n,".",sep=""),names(out))
-	out
-}
-
-SC_GE_a_fn<-function(GE){
-	sum_fn(GE)/sum(GE[,"N"])
-}
-
-SC_GE_a<-foreach(d=lapply(SC_GE_list,SC_GE_a_fn),n=names(SC_GE_list),.combine=c)%do%{ 
-	out<-d
-	names(out)<-gsub("^",paste(n,".",sep=""),names(out))
-	out
-}
-
-SC_d_sum<-c(SIR_sum,SC_GE_sum)
-SC_d_a<-c(SIR_a,SC_GE_a)
-
-chisq_chart <- function(d=SC_d_list,n=6,decreasing=TRUE){
-	chart(d[head(order(sapply(d,chisq_fn),decreasing=decreasing),n)],lwd=5)
-}
-
-# UK level density
-UK_GE_list <- list(GE2005=GE2005,
+UK_list <- list(GE2005=GE2005,
 	GE2010=GE2010,
-	GE2015=GE2015
+	GE2015=GE2015,
+	VSR2011=VSR2011,
+	EUR2016=EUR2016
 )
 
+metadata <- function(
+	l
+){
+	foreach(b=lapply(l,metadata_b),
+		n=names(l),
+		.combine=rbind
+	)%do%{
+		rownames(b)<-paste(n,".",rownames(b),sep="")
+		b
+	}
+}
+
+metadata_b <- function(
+	b
+){
+	out_d<-ballot_funk(b)
+	
+	out_logchisq <- unlist(
+		lapply(
+			out_d,
+			logchisq_fn
+		)
+	)
+
+	out_skewness <- unlist(
+		ballot_funk(
+			b,
+			FUN=skewness
+		)
+	)
+
+	out_sum<-unlist(ballot_funk(
+		b,
+		FUN=return,
+		SAMPLE_FUN=function(V,N)sum(V)
+	))
+
+	out_a<-unlist(ballot_funk(
+		b,
+		FUN=return,
+		SAMPLE_FUN=function(V,N)sum(V)/sum(N)
+	))
+	cbind(
+		logchisq=out_logchisq,
+		skewness=out_skewness,
+		sum=out_sum,
+		a=out_a
+	)
+}
+
+logchisq_fn <- function(d){
+	log(sum((d$y-dnorm(d$x))^2))
+}
+
+chisq_chart <- function(d=ballot_list_funk(SC_list),n=6,decreasing=TRUE){
+	chart(d[head(order(sapply(d,logchisq_fn),decreasing=decreasing),n)],lwd=2)
+	x<-seq(-4,4,0.05)
+	lines(x,dnorm(x),lwd=2,col=8,lty=2)
+}
+
+scatter_chart <- function(m,...){
+	a<-m[,"a"]
+	lc<-m[,"logchisq"]
+	na<-(a-mean(a))/sd(a)
+	nlc<-(lc-mean(lc))/sd(lc)
+	k<-head(order(na+nlc,decreasing=TRUE))
+	plot(	x=lc[-k],
+		y=a[-k],
+		xlab="log chisq deviation from Gaussian",
+		ylab="turnout",
+		xlim=limits(lc),
+		ylim=limits(a),
+		col=8,
+		pch=8,
+		...
+	)
+	points(	x=lc[k],
+		y=a[k],
+		col=seq(length(k)),
+		pch=seq(length(k)),
+		...
+	)
+	legend(	"bottomleft",
+		legend=rownames(m)[k],
+		col=seq(length(k)),
+		pch=seq(length(k))
+	)
+}
--- electoralAnalysis/load_data.r
+++ electoralAnalysis/load_data.r
@@ -2,18 +2,20 @@
 require(gdata)
 options(stringsAsFactors=FALSE)
 
+banned_parties = "(^Ind|^$|^Speaker$)"
+
 # ballot trimming function.
 # Independent candidates aren't a party
 # Parties need at least 7 candidates
 trim_ballot<-function(b){
-	n<-colSums(b[,sapply(b[1,],is.numeric)])!=0
+	n<-sapply(b[1,],is.numeric)
+	print(head(n))
 	n[sapply(names(n)[n],
 		function(x){
-			sum(b[,x]!=0)<7
+			sum(b[,x]==0)>7
 		}
 	)]<-FALSE
-	n[grep("^Ind.$",names(n))]<-FALSE
-	n[grep("^Independent$",names(n))]<-FALSE
+	print(head(n))
 	b[,c(colnames(b)[!sapply(b[1,],is.numeric)],
 		names(n)[n]
 	)]
@@ -27,30 +29,111 @@ trim_number <- function(s){
 	as.numeric(gsub("^$","0",gsub(",* *","",s)))
 }
 
+# (b) ballot needs counting areas as rownames
+# (r) result neds format [,c("keyname","party","votes")] 
+result_sorter <- function(b,r){
+	for(party in levels(factor(r[,2]))){
+		n<-grep(party,r[,2])
+		if(length(n)>7){
+			b<-cbind(b,0)
+			colnames(b)[ncol(b)]<-party
+			b[r[n,1],ncol(b)]<-trim_number(r[n,3])
+		}
+	}
+	b
+}
+
 # Load data
 
 # Load GE2005 data
 if(!exists("GE2005")){
-	GE2005<-read.xls(sheet=3,
+	GE2005_reg<-read.xls(sheet=3,
 		"data/org/General-election-2005---postal-voting,-proxies-and-spoilts.xls"
-	)[-seq(1,4),seq(1,4)]
-	colnames(GE2005)<-c("Region","Name","N","V")
-	GE2005$N<-as.numeric(GE2005$N)
-	GE2005$V<-as.numeric(GE2005$V)
+	)[c(-seq(1,4)),1]
+	
+	
+#	colnames(GE2005_old)<-c("Region","Name","N","V")
+#	GE2005_old$N<-trim_number(GE2005_old$N)
+#	GE2005_old$V<-trim_number(GE2005_old$V)
+#	rownames(GE2005_old)<-trim_white_space(GE2005_old[,2])
+#	GE2005_old<-GE2005_old[,-2]
+
+	# read constituency/electorate, party, vote tally, candidate name columns
+	GE2005R<-read.xls(
+		"data/org/Generalelection2005_A-Zconstituencyresults_18784-13893__E__N__S__W__.xls"
+	)[-seq(1,7),c(1,4,5,3)]
+
+	# read constituency start indices
+	index<-grep("\\[\\d*\\]",GE2005R[,1])
+	
+	# read names
+	GE2005_rn<-GE2005R[index,1]
+
+	# read electorate
+	GE2005_N1<-GE2005R[index+1,1]
+	GE2005_N1[GE2005_N1==""]<-GE2005R[index[GE2005_N1==""]+2,1]
+	GE2005_N<-trim_number(GE2005_N1)
+	rm(GE2005_N1)
+
+	# read constituency end indices
+	index2<-grep("^-+$",GE2005R[,3])
+
+	# strip unused rows
+	GE2005R<-GE2005R[
+		-c(	unlist(lapply(	seq(length(index)-1),
+					function(x){
+						seq(index2[x],index[x+1]-1)
+					}
+				)
+			),
+			seq(index2[length(index2)],nrow(GE2005R))
+		),
+	]
+	
+	# convert data
+	GE2005R[,2]<-factor(trim_white_space(GE2005R[,2]))
+	GE2005R[,3]<-trim_number(GE2005R[,3])
+
+	# correct indices
+	index<-grep("\\[\\d*\\]",GE2005R[,1])
+	rm(index2)
+
+	# read turnout data
+	key<-function(i)sum(i>=index)
+	GE2005_V <- as.vector(unlist(lapply(split(GE2005R[,3],
+				sapply(seq(nrow(GE2005R)),
+					key)
+				),
+				sum
+			)
+		)
+	)
+	parties<-levels(GE2005R[,2])
+	parties<-parties[-grep(banned_parties,parties)]
+	GE2005i<-data.frame(Region=as.factor(GE2005_reg),
+		N=GE2005_N,
+		V=as.vector(GE2005_V),
+		row.names=GE2005_rn
+	)
+	GE2005<-foreach(
+		p=parties,
+		.combine=cbind,
+		.init=GE2005i
+	)%do%{
+		i<-grep(p,GE2005R[,2])
+		out<-rep(0,length(GE2005_N))
+		out[sapply(i,key)]<-GE2005R[i,3]
+		out
+	}
+	colnames(GE2005)[-seq(1,3)]<-parties
+#	GE2005<-trim_ballot(GE2005)
 }
 
 if(!exists("GE2005S")){
-	GE2005R<-read.xls(
-		"data/org/Generalelection2005_A-Zconstituencyresults_18784-13893__E__N__S__W__.xls"
-	)[-seq(1,7),seq(1,5)]
-	GE2005S<-cbind(GE2005[GE2005$Region=="Scotland",],
-		SNP=as.numeric(gsub(",","",(GE2005R[GE2005R$X.2=="SNP",])[,5]))
-	)
+	GE2005S<-GE2005[GE2005$Region=="Scotland",colSums(GE2005[GE2005$Region=="Scotland",]!=0)>5]
 	GE2005S<-cbind(GE2005S,
 		nonSNP=GE2005S$V-GE2005S$SNP
 	)
-	
-	rm("GE2005R")
 }
 
 # Load GE2010 data
@@ -60,11 +143,11 @@ if(!exists("GE2010")){
 	)[-651,-c(1,4)]
 	GE2010[is.na(GE2010)]<-0
 	colnames(GE2010)[c(3,4)]<-c("N","V")
-	GE2010<-trim_ballot(GE2010)
+#	GE2010<-trim_ballot(GE2010)
 }
 
 if(!exists("GE2010S")){
-	GE2010S<-trim_ballot(GE2010[GE2010$Region=="Scotland",])
+	GE2010S<-GE2010[GE2010$Region=="Scotland",colSums(GE2010[GE2010$Region=="Scotland",]!=0)>5]
 	GE2010S<-cbind(GE2010S,
 		nonSNP=GE2010S$V-GE2010S$SNP
 	)
@@ -77,10 +160,15 @@ if(!exists("GE2015")){
 		"data/org/2015-UK-General-election-data-collated-results-Constituency.csv"
 	)[,c(2,3,5,6)]
 	colnames(GE2015)[c(3,4)]<-c("N","V")
+	rownames(GE2015)<-trim_white_space(GE2015[,1])
+	GE2015<-GE2015[,-1]
+	GE2015R<-read.csv(
+		"data/org/2015-UK-General-election-data-collated-results-Results.csv"
+	)[-3972,c(1,2,4,3,6)]
 }
 
 if(!exists("GE2015S")){
-	GE2015R<-read.csv("data/org/2015-UK-General-election-data-collated-results-Results.csv")
+#	GE2015R<-read.csv("data/org/2015-UK-General-election-data-collated-results-Results.csv")
 	GE2015S<-cbind(GE2015[grep("S",GE2015$Constituency.ID),],
 		SNP=GE2015R[grep("SNP",GE2015R$Description.on.ballot.paper),]$Votes
 	)
@@ -88,12 +176,11 @@ if(!exists("GE2015S")){
 		nonSNP=GE2015S$V-GE2015S$SNP
 	)
 
-	rm("GE2015R")
 }
 
 # Load SIR2014 data
 if(!exists("SIR2014")){
-	SIR2014<-read.csv("data/SIR2014.csv")
+	SIR2014<-read.csv("data/SIR2014.csv")[,-c(1,2,6,7,seq(9,15))]
 }
 
 # Load SP2007 data
@@ -101,11 +188,11 @@ if(!exists("SP2007")){
 	SP2007<-read.xls(
 		"data/org/Scotland-Parl-Elec-2007-Turnout-Electorate-Constituency.xls"
 	)[seq(2,74),c(2,3,5)]
-	colnames(SP2007)<-c("Name","N","V")
-	SP2007$N<-as.numeric(gsub(",","",SP2007$N))
-	SP2007$V<-as.numeric(gsub(",","",SP2007$V))
 	rownames(SP2007)<-trim_white_space(SP2007[,1])
 	SP2007<-SP2007[,-1]
+	colnames(SP2007)<-c("N","V")
+	SP2007$N<-trim_number(SP2007$N)
+	SP2007$V<-trim_number(SP2007$V)
 	SP2007R<-read.xls("data/org/Scotland-Parl-2007-Candidate-Constituency.xls")[-1,-c(1,6)]
 	colnames(SP2007R)<-c("Name","Candiate","Party","V")
 	SP2007R[,1]<-trim_white_space(SP2007R[,1])
@@ -116,21 +203,11 @@ if(!exists("SP2007")){
 			SP2007R[i,1]<-SP2007R[i-1,1]
 		}
 	}
-	for(party in levels(factor(SP2007R[,3]))){
-		if(length(n<-grep(party,SP2007R[,3]))>5){
-			SP2007<-cbind(SP2007,
-				rep(0,nrow(SP2007))
-			)
-			colnames(SP2007)[ncol(SP2007)]<-party
-			SP2007[SP2007R[n,1],ncol(SP2007)]<-SP2007R[n,4]
-		}
-	}
+	SP2007<-result_sorter(SP2007,SP2007R[,c(1,3,4)])
+	SP2007<-cbind(SP2007,
+		nonSNP=SP2007$V-SP2007$SNP
+	)
 	SP2007<-trim_ballot(SP2007)
-	#SP2007<-cbind(SP2007,SNP=as.numeric(gsub(",","",SP2007R[grep("SNP",SP2007R[,1]),2])))
-	#SP2007<-cbind(SP2007,
-	#	nonSNP=SP2007$V-SP2007$SNP
-	#)
-
 }
 
 #Load SP2011 data
@@ -138,8 +215,10 @@ if(!exists("SP2011")){
 	SP2011<-read.xls(
 		"data/org/SP-2011-results-flat-file-WEB.xlsx"
 	)[,-c(3,5,6,11,12)]
-	colnames(SP2011)[c(2,3)]<-c("N","V")
-	for(i in seq(2,ncol(SP2011))){
+	rownames(SP2011)<-SP2011[,1]
+	SP2011<-SP2011[,-1]
+	colnames(SP2011)[c(1,2)]<-c("N","V")
+	for(i in seq(ncol(SP2011))){
 		SP2011[,i]<-trim_number(SP2011[,i])
 	}
 	SP2011<-cbind(SP2011,
@@ -153,7 +232,9 @@ if(!exists("SP2016")){
 		"data/org/RESULTS Constituency - Electoral Data and Results - Scottish Parliament elections - May 2016.csv"
 	)[seq(2,74),c(2,3,4,6,10,11,12,13)]
 	colnames(SP2016)<-c("Name","Region","N","V","CON","LAB","LIB","SNP")
-	for(i in seq(3,ncol(SP2016))){
+	rownames(SP2016)<-SP2016[,1]
+	SP2016<-SP2016[,-1]
+	for(i in seq(2,ncol(SP2016))){
 		SP2016[,i]<-trim_number(SP2016[,i])
 	}
 	SP2016<-cbind(SP2016,
