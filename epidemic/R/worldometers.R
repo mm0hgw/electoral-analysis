478cbdbb8dcc5afc76d262b16922dae49e88c85b
Dale <dale@piratepress.org>
2020-03-28 20:34:31 +0000

    epidemic stuff

---
+++ epidemic/R/worldometers.R
@@ -0,0 +1,28 @@
+
+
+initColIDs <- c('Date','Total.Cases','Active.Cases','Deaths')
+colIDs <- c("Date", "Total.Cases", "Active.Cases", "Deaths", "Inactive.Cases", 
+"Recoveries", "New.Cases", "New.Inactive.Cases", "New.Deaths", 
+"New.Recoveries")
+
+calculateRateOfChange <- function(x){
+	n <- length(x)
+	c(0,x[-1]-x[-n])
+}
+
+calculateInactiveRecoveriesAndNew <- function(tab){
+	stopifnot(all(initColIDs %in% colnames(tab)))
+	x <- tab[,initColIDs]
+	xCols <- nrow(x)
+	x$Inactive.Cases <- x$Total.Cases - x$Active.Cases
+	x$Recoveries <- x$Inactive.Cases - x$Deaths
+	x$New.Cases <- calculateRateOfChange(x$Total.Cases)
+	x$New.Inactive.Cases <- calculateRateOfChange(x$Inactive.Cases)
+	x$New.Deaths <- calculateRateOfChange(x$Deaths)
+	x$New.Recoveries <- calculateRateOfChange(x$Recoveries)
+	x
+}
+
+logFilter <- function(x){
+	sapply(seq(nrow(x)),function(n)all(x[n,]>0))
+}
