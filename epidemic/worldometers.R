de1d3be4c109b2bb0387cffa1cfa241c15990b7e
Dale <dale@piratepress.org>
2020-03-29 18:16:38 +0100

    KR

--- epidemic/worldometers.R
+++ epidemic/worldometers.R
@@ -1,6 +1,7 @@
 
 source("R/worldometers.R")
 
+
 UKRaw <- read.csv("data/worldometers_UK.csv")
 UK <- calculateInactiveRecoveriesAndNew(UKRaw)
 
@@ -8,9 +9,23 @@ write.csv(UK, file = "UK.csv")
 
 UKNewVSActive <- UK[, c("Active.Cases", "New.Cases")]
 
-key <- logFilter(UKNewVSActive)
+UKDays <- 16 +seq_along(UK$Total.Cases)
 
 png("UK.png", 1024, 768)
-plot(UKNewVSActive[key, ], log = "xy", type = "b")
-text(UKNewVSActive[key, ], labels = gsub("2020-", "", UK$Date[key]), pos = 2, cex = 0.75)
+plot(UKNewVSActive, log = "xy", type = "b")
+text(UKNewVSActive, labels = UKDays, pos = 2, cex = 0.75)
+dev.off()
+
+KRRaw <- read.csv("data/worldometers_KR.csv")
+KR <- calculateInactiveRecoveriesAndNew(KRRaw)
+
+write.csv(KR, file = "KR.csv")
+
+KRNewVSActive <- KR[, c("Active.Cases", "New.Cases")]
+
+KRDays <- 27 +seq_along(KR$Total.Cases)
+
+png("KR.png", 1024, 768)
+plot(KRNewVSActive, log = "xy", type = "b")
+text(KRNewVSActive, labels = KRDays, pos = 2, cex = 0.75)
 dev.off()
