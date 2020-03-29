7a22b30629e8d817f92b13cd104df6f5a6430c3a
Dale <dale@piratepress.org>
2020-03-29 18:26:57 +0100

    trunky

--- epidemic/worldometers.R
+++ epidemic/worldometers.R
@@ -9,7 +9,7 @@ write.csv(UK, file = "UK.csv")
 
 UKNewVSActive <- UK[, c("Active.Cases", "New.Cases")]
 
-UKDays <- 16 +seq_along(UK$Total.Cases)
+UKDays <- 16 + seq_along(UK$Total.Cases)
 
 png("UK.png", 1024, 768)
 plot(UKNewVSActive, log = "xy", type = "b")
@@ -23,7 +23,7 @@ write.csv(KR, file = "KR.csv")
 
 KRNewVSActive <- KR[, c("Active.Cases", "New.Cases")]
 
-KRDays <- 27 +seq_along(KR$Total.Cases)
+KRDays <- 27 + seq_along(KR$Total.Cases)
 
 png("KR.png", 1024, 768)
 plot(KRNewVSActive, log = "xy", type = "b")
