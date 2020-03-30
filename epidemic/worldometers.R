e5771e6f0e3ccafb57515ed367fe2f0d1726058d
Dale <dale@piratepress.org>
2020-03-30 13:16:36 +0100

    key

--- epidemic/worldometers.R
+++ epidemic/worldometers.R
@@ -8,19 +8,18 @@ processCountry <- function(country, delta) {
     
     rawFile <- paste0("data/worldometers_", country, ".csv")
     rawTab <- read.csv(rawFile)
+
     tab <- calculateInactiveRecoveriesAndNew(rawTab)
-    
     write.csv(tab, file = paste0(country, ".csv"))
     
-    newVSActive <- tab[, c("Active.Cases", "New.Cases")]
-    
-    days <- delta + seq_along(tab$Total.Cases)
+    key <- (tab$Active.Cases != 0) & (tab$New.Cases != 0)
+    newVSActive <- tab[key, c("Active.Cases", "New.Cases")]
+    days <- delta + seq_along(tab$Total.Cases)[key]
     
     png(paste0(country, ".png"), 1024, 768)
     plot(newVSActive, log = "xy", type = "b")
     text(newVSActive, labels = days, pos = 2, cex = 0.75)
     dev.off()
-    
 }
 
 lapply(seq_along(countries), function(x) processCountry(names(countries)[x], countries[x]))
